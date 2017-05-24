23b3c69d5e41693231540b8b624cd2c343eab65a
Jim Krooskos <krooskos@gmail.com>
2017-05-24 06:36:59 -0700

    Add test for required fields for char and player name.

--- tests/test_player_tools.py
+++ tests/test_player_tools.py
@@ -10,7 +10,7 @@ from utils import set_input_value
 
 
 def test_player_wizard(browser):
-    """Test player wizard using required values."""
+    """A user should be able to navigate through the player wizard."""
     print('A user should be able to navigate through the player wizard.')
 
     # AC homepage
@@ -45,7 +45,7 @@ def test_player_wizard(browser):
 
 
 def test_attributes_required(browser):
-    """Test player wizard attribures are required fields."""
+    """A user should be required to add attribute values."""
     print('A user should be required to add attribute values.')
 
     # AC homepage
@@ -77,6 +77,37 @@ def test_attributes_required(browser):
     # With required fields, the finish button should not be visible
     with pytest.raises(NoSuchElementException) as excinfo:
         xpath = "//button[contains(text(), 'Finish')]"
+        browser.find_element_by_xpath(xpath)
+
+    assert 'no such element' in str(excinfo)
+
+
+def test_name_required(browser):
+    """A user should be required to enter a char and player name."""
+    print('A user should be required to enter a char and player name.')
+
+    # AC homepage
+    click_link('Start Playing', browser)
+
+    # Get started with the wizard
+    click_button('Get Started', browser)
+
+    # Select type player
+    click_radio('characterPlayerType', browser)
+
+    # Navigate to the next step
+    click_button('Next', browser)
+
+    xpath = "//div[@class='col-md-2 col-padded']//span[contains(text(), 'Required')]"
+    errors = WebDriverWait(browser, 30).until(
+        EC.presence_of_all_elements_located((By.XPATH, xpath)))
+
+    # There should be one required text for each attribute
+    assert len(errors) == 2
+
+    # With required fields, the finish button should not be visible
+    with pytest.raises(NoSuchElementException) as excinfo:
+        xpath = "//button[contains(text(), 'Next')]"
         browser.find_element_by_xpath(xpath)
 
     assert 'no such element' in str(excinfo)
