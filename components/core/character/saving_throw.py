db5cf140a767a084124a2d933ae4ac2754df7640
Jim Krooskos <krooskos@gmail.com>
2018-02-03 12:38:26 -0800

    Add saving throw

---
+++ components/core/character/saving_throw.py
@@ -0,0 +1,19 @@
+"""Saving Throw components."""
+
+from modules.element import Element, Component
+
+
+class SavingThrowEditModal(Component):
+    """Definition of Saving Throw edit modal component."""
+
+    name = Element(id_='savingThrowEditNameInput')
+    modifier = Element(id_='savingThrowEditModifierButton')
+    proficiency = Element(id_='savingThrowEditProficiencyCheckbox')
+    done = Element(id_='savingThrowEditDoneButton')
+
+
+class SavingThrowTable(Component):
+    """Definition of savingsThrow edit modal componenet."""
+
+    sort_by_saving_throw = Element(id_='savingThrowTableSortSavingThrowSpan')
+    table = Element(id_='savingThrowTable')
