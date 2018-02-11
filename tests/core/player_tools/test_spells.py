13baa66c5e6b24006659e6e8bdbbeb02c1677467
alejandroke <alejandrokeperez@gmail.com>
2018-02-11 11:48:54 -0800

    Add spells and spell stats components

--- tests/core/player_tools/test_spells.py
+++ tests/core/player_tools/test_spells.py
@@ -1,1 +1,15 @@
 """UAT test file for Adventurer's Codex player tools spells module."""
+from components.core.character.spells import SpellStatsEdit
+
+
+
+def test_navigate_spells(player_wizard, browser): # noqa
+    """As a player, spell stats can be added, deleted, and edited"""
+    print('As a player, spell stats can be added, deleted, and edited')
+
+    spells = SpellStatsEdit(browser)
+
+    spells.spell_Stats_Table.click()
+
+    spells.spell_attack_bonus.click()
+    assert tabs.stats_label.text == '1'
