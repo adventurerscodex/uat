7b8c0edba1e01a02689aba1ec59e77f8fec9f19a
Jim Krooskos <krooskos@gmail.com>
2018-03-10 06:42:57 -0800

    Add expected condition for table cell updated

--- expected_conditions/conditions.py
+++ expected_conditions/conditions.py
@@ -1,3 +1,7 @@
+"""Custom expected conditions."""
+
+from utils import utils as ut
+
 class url_in_new_tab_matches:  # noqa
     """Wait until new tab contains redirected url."""
 
@@ -13,7 +17,7 @@ class table_has_data:  # noqa
     """Wait until table has data."""
 
     def __init__(self, table):
-        """Init escpected condition."""
+        """Init expected condition."""
         self.table = table
 
     def __call__(self, driver):
@@ -21,3 +25,33 @@ class table_has_data:  # noqa
         tds = self.table.find_elements_by_tag_name('td')
 
         return len(tds) > 1
+
+class table_cell_updated: # noqa
+    """Wait until cell data has updated."""
+
+    def __init__(self, table, header, expected, table_attr, row_number):
+        """Init expected condition.
+
+        :table: Table component
+        :header: Name of the table header
+        :expected: Expected value cell value should be
+        :table_attr: Name of the attr of table reference, usually 'table'
+        :row_number: Row number to retrieve
+        """
+        self.table = table
+        self.header = header
+        self.expected = expected
+        self.table_attr = table_attr
+        self.row_number = row_number
+
+    def __call__(self, driver):
+        """Test if col value has updated."""
+        row = ut.get_table_row(
+            self.table,
+            self.table_attr,
+            row_number=self.row_number
+        )
+
+        actual = getattr(row, self.header)
+
+        return actual == self.expected
