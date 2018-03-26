553e02f3fe8d5ed5bead99b01d4627f3e326e6b3
Jim Krooskos <krooskos@gmail.com>
2018-03-25 17:38:13 -0700

    Remove unused imports

--- tests/core/player_tools/test_inventory.py
+++ tests/core/player_tools/test_inventory.py
@@ -1,6 +1,5 @@
 """UAT test file for Adventurer's Codex player tools inventory module."""
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC # noqa
 from selenium.webdriver.support.ui import WebDriverWait
 
