b1e847bb67e7fe22bba39eeefaba4bd9c930012a
Jim Krooskos <krooskos@gmail.com>
2018-04-27 05:01:20 -0700

    Add modal finished closing to delete feature

--- tests/core/player_tools/test_skills.py
+++ tests/core/player_tools/test_skills.py
@@ -93,17 +93,12 @@ def test_delete_feature(player_wizard, browser): # noqa
     )
     feature.add.click()
 
+    WebDriverWait(browser, 10).until(
+        modal_finished_closing(feature.modal_div_id)
+    )
+
     rows = ut.get_table_rows(features_table, 'table', values=False)
-    WebDriverWait(browser, 10).until(
-        EC.invisibility_of_element_located(
-            (By.CLASS_NAME, 'modal-backdrop fade')
-        )
-    )
-    WebDriverWait(browser, 10).until(
-        EC.invisibility_of_element_located(
-            (By.ID, 'addFeature')
-        )
-    )
+
     rows[0][2].click()
     rows = ut.get_table_rows(features_table, 'table', values=False)
 
