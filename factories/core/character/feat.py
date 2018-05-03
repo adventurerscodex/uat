640ba6a6ade32e5a1c7fd043a8c83929ccf812c5
Charlie Torres <charlie@chartorres.com>
2018-05-03 16:23:54 -0700

    Add feat factory

---
+++ factories/core/character/feat.py
@@ -0,0 +1,30 @@
+"""Feat factory."""
+
+import factory.fuzzy
+
+from factories.fixtures import Fixtures
+
+
+class Feat:
+    """Definition of feat model."""
+
+    def __init__(
+        self,
+        name,
+        description,
+    ):
+        """Initialize feat instance."""
+        self.name = name,
+        self.description = description
+
+
+class FeatFactory(factory.Factory):
+    """Definition of feat factory."""
+
+    class Meta:
+        """Bind model to factory."""
+
+        model = Feat
+
+    name = factory.fuzzy.FuzzyText(length=125, prefix='Feat_')
+    description = factory.fuzzy.FuzzyText(length=150,prefix='Description_')
