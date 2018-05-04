83854c1d413a1928e957602c8da5834aa72ac0db
alexp <alejandrokeperez@gmail.com>
2018-05-04 00:46:23 -0700

    Point of interest factory

---
+++ factories/core/dm/pointofinterest.py
@@ -0,0 +1,37 @@
+"""Point Of Interest Factory"""
+
+import factory
+
+from factories.fixtures import random_wordlist
+
+
+class PointOfInterest:
+    """Definition of Point Of Interest model."""
+
+    def __init__(
+        self,
+        name,
+        description,
+    ):
+        """Initialize Point Of Interest instance."""
+        self.name = name
+        self.description = description
+
+
+class PointOfInterestFactory(factory.Factory):
+    """Definition of Point Of Interest factory."""
+
+    class Meta:
+        """Bind model to factory."""
+
+        model = PointOfInterest
+
+    name = factory.Faker(
+        'word',
+        ext_word_list=random_wordlist(prefix='PointOfInterest_', max_length=40)
+    )
+
+    description = factory.Faker(
+        'text',
+        max_nb_chars=150
+    )
