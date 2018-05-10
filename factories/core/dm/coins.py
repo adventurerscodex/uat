0b98251d4a5eb5a2099488ac13ccac6d6133173a
Sean K <sean.kipperman@gmail.com>
2018-05-10 16:11:02 -0700

    Coins factory (#94)
    * Add Coins Factory
    
    * Remove unneccessary import statements from Coins Factory
    
    * Create dm directory in factories/core/, move coins.py to dm

---
+++ factories/core/dm/coins.py
@@ -0,0 +1,42 @@
+"""Coins factory."""
+
+import factory
+
+
+class Coins:
+    """Definition of coins model."""
+
+    def __init__(
+        self,
+        platinum,
+        gold,
+        electrum,
+        silver,
+        copper,
+
+    ):
+        """Initialize coins instance."""
+        self.platinum = platinum
+        self.gold = gold
+        self.electrum = electrum
+        self.silver = silver
+        self.copper = copper
+
+
+class CoinsFactory(factory.Factory):
+    """Definition of coins factory."""
+
+    class Meta:
+        """Bind model to factory."""
+
+        model = Coins
+
+    platinum = factory.Faker('random_int', min=0, max=1000000)
+
+    gold = factory.Faker('random_int', min=0, max=1000000)
+
+    electrum = factory.Faker('random_int', min=0, max=1000000)
+
+    silver = factory.Faker('random_int', min=0, max=1000000)
+
+    copper = factory.Faker('random_int', min=0, max=1000000)
