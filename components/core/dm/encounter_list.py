6040ead78a37ada7645fc282e0958f2a8d1b0288
alexp <alejandrokeperez@gmail.com>
2018-07-01 01:41:08 -0700

    Added fixture and encounter list test

--- components/core/dm/encounter_list.py
+++ components/core/dm/encounter_list.py
@@ -8,6 +8,8 @@ class EncounterList(Component):
 
     add_plus_icon_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[1]/div/div/div/div/div' # noqa
     add_encounter_detail_placeholder_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[2]/div' # noqa
+    added_list_encounter_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[1]/div/div/nested-list/div/a/span[1]' # noqa
 
     add_plus_icon = Element(xpath=add_plus_icon_xpath)
     add_encounter_detail_placeholder = Element(xpath=add_encounter_detail_placeholder_xpath)
+    added_list_encounter = Element(xpath=added_list_encounter_xpath)
--- tests/core/dm_tools/test_encounters.py
+++ tests/core/dm_tools/test_encounters.py
@@ -1,1 +1,33 @@
 """UAT test file for Adventurer's Codex dm tools encounters module."""
+from components.core.dm.encounter_list import EncounterList
+from components.core.dm.tabs import DMTabs
+from components.core.dm.encounter_add_edit_modal import EncounterAddEditModal
+
+
+def test_full_encounter(dm_wizard, browser):
+    """This test will create a full encounter list"""
+    print('This test will create a full encounter list')
+
+    tabs = DMTabs(browser)
+
+    tabs.encounters.click()
+
+    assert tabs.encounters_label.text.strip() == 'Encounters'
+
+    encounter_list = EncounterList(browser)
+
+    encounter_list.add_plus_icon.click()
+
+    encounter_add_edit_modal = EncounterAddEditModal(browser)
+
+    encounter_add_edit_modal.encounter_name = 'Encounters'
+    encounter_add_edit_modal.location = 'Location'
+    encounter_add_edit_modal.environment.click()
+    encounter_add_edit_modal.maps_and_images.click()
+    encounter_add_edit_modal.points_of_interest.click()
+    encounter_add_edit_modal.non_player_characters.click()
+    encounter_add_edit_modal.monsters.click()
+    encounter_add_edit_modal.treasure.click()
+    encounter_add_edit_modal.done.click()
+
+    assert encounter_list.added_list_encounter.text.strip() == 'Encounters'
