cc05e5a9b11436fdc24bfacb8015fa3152a620d8
alexp <alejandrokeperez@gmail.com>
2018-07-01 18:21:08 -0700

    read aloud text saving changes

--- components/core/dm/read_aloud_text.py
+++ components/core/dm/read_aloud_text.py
@@ -6,12 +6,18 @@ from component_objects import Component, Element
 class ReadAloudTextAddModal(Component):
     """Definition of Read Aloud Text Add component."""
 
-    name_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[2]/encounter-detail/div/div[2]/player-text-section/table/thead/tr/th[1]' # noqa
-    description_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[2]/encounter-detail/div/div[2]/player-text-section/table/thead/tr/th[2]' # noqa
-    add_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[2]/encounter-detail/div/div[2]/player-text-section/table/thead/tr/th[4]/a/i' # noqa
+    name_xpath = '//*[@id="addPlayerText"]/div/div/div[2]/form/div[1]/div[1]/input' # noqa
+    description_xpath = '//*[@id="addPlayerText"]/div/div/div[2]/form/div[1]/div[2]/textarea' # noqa
+    add_plus_icon_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[2]/encounter-detail/div/div[2]/player-text-section/table/thead/tr/th[4]/a/i' # noqa
+    add_new_point_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[2]/encounter-detail/div/div[2]/player-text-section/table/tbody/tr/td' # noqa
+    remove_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[2]/encounter-detail/div/div[2]/player-text-section/table/tbody/tr/td[5]/a/i'# noqa
+    add_xpath = '//*[@id="addPlayerText"]/div/div/div[2]/form/div[2]/button'
 
     name = Element(xpath=name_xpath)
     description = Element(xpath=description_xpath)
+    add_plus_icon = Element(xpath=add_plus_icon_xpath)
+    add_new_point = Element(xpath=add_new_point_xpath)
+    remove = Element(xpath=remove_xpath)
     add = Element(xpath=add_xpath)
 
 
@@ -19,7 +25,7 @@ class ReadAloudTextModalTabs(Component):
     """Definition of Read Aloud Text Tab component."""
 
     preview_xpath = '//*[@id="viewWeapon"]/div/div/div[2]/ul/li[1]/a/b'
-    edit_xpath = '//*[@id="viewWeapon"]/div/div/div[2]/ul/li[2]/a/b'
+    edit_xpath = '//*[@id="viewWeapon"]/div/div/div[2]/ul/li[2]'
 
     preview = Element(xpath=preview_xpath)
     edit = Element(xpath=edit_xpath)
@@ -47,3 +53,17 @@ class ReadAloudTextEditModal(Component):
     name = Element(xpath=name_xpath)
     description = Element(xpath=description_xpath)
     done = Element(xpath=done_xpath)
+
+
+class ReadAloudTextTable(Component):
+    """Definition of Read Aloud Text Table component."""
+
+    name_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[2]/encounter-detail/div/div[2]/player-text-section/table/tbody/tr/td[1]' # noqa
+    description_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[2]/encounter-detail/div/div[2]/player-text-section/table/tbody/tr/td[3]' # noqa
+    first_row_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[2]/encounter-detail/div/div[2]/player-text-section/table/tbody/tr'
+    trash_xpath = '//*[@id="encounter-tab"]/encounter/div/div/div[2]/div[2]/encounter-detail/div/div[2]/player-text-section/table/tbody/tr/td[5]/a/i' # noqa
+
+    name = Element(xpath=name_xpath)
+    description = Element(xpath=description_xpath)
+    first_row = Element(xpath=first_row_xpath)
+    trash = Element(xpath=trash_xpath)
--- tests/core/dm_tools/test_encounters.py
+++ tests/core/dm_tools/test_encounters.py
@@ -1,33 +1,56 @@
 """UAT test file for Adventurer's Codex dm tools encounters module."""
-from components.core.dm.encounter_list import EncounterList
-from components.core.dm.tabs import DMTabs
-from components.core.dm.encounter_add_edit_modal import EncounterAddEditModal
+from components.core.dm.read_aloud_text import ReadAloudTextAddModal
+from components.core.dm.read_aloud_text import ReadAloudTextTable
+from components.core.dm.read_aloud_text import ReadAloudTextEditModal
+from components.core.dm.read_aloud_text import ReadAloudTextModalTabs
+from factories.core.dm.read_aloud_text import ReadAloudTextFactory
 
 
-def test_full_encounter(dm_wizard, browser):
-    """This test will create a full encounter list"""
-    print('This test will create a full encounter list')
+def test_add_read_aloud_text(dm_wizard, encounter_all_sections, browser):
+    """As a dm, I can add read aloud text to an encounter and the data persists"""
+    print('As a dm, I can add read aloud text to an encounter and the data persists')
 
-    tabs = DMTabs(browser)
+    read_aloud_text = ReadAloudTextAddModal(browser)
+    read_aloud_table = ReadAloudTextTable(browser)
 
-    tabs.encounters.click()
+    read_aloud_text.add_plus_icon.click()
+    stub = ReadAloudTextFactory.stub()
 
-    assert tabs.encounters_label.text.strip() == 'Encounters'
+    read_aloud_text.name = stub.name
+    read_aloud_text.description = stub.description
+    read_aloud_text.add.click()
 
-    encounter_list = EncounterList(browser)
+    assert read_aloud_table.name.text.strip() == stub.name
+    assert read_aloud_table.description.text.strip() == stub.description
 
-    encounter_list.add_plus_icon.click()
 
-    encounter_add_edit_modal = EncounterAddEditModal(browser)
+def test_edit_read_aloud_text(dm_wizard, encounter_all_sections, browser):
+	"""As a dm, I can edit a read aloud text in an encounter and the data persists and delete a read aloud text in an encounter""" #noqa
+	print('As a dm, I can edit a read aloud text in an encounter and the data persists and delete a read aloud text in an encounter') #noqa
+	read_aloud_text = ReadAloudTextAddModal(browser)
+	read_aloud_table = ReadAloudTextTable(browser)
 
-    encounter_add_edit_modal.encounter_name = 'Encounters'
-    encounter_add_edit_modal.location = 'Location'
-    encounter_add_edit_modal.environment.click()
-    encounter_add_edit_modal.maps_and_images.click()
-    encounter_add_edit_modal.points_of_interest.click()
-    encounter_add_edit_modal.non_player_characters.click()
-    encounter_add_edit_modal.monsters.click()
-    encounter_add_edit_modal.treasure.click()
-    encounter_add_edit_modal.done.click()
+	read_aloud_text.add_plus_icon.click()
+	stub = ReadAloudTextFactory.stub()
 
-    assert encounter_list.added_list_encounter.text.strip() == 'Encounters'
+	read_aloud_text.name = stub.name
+	read_aloud_text.description = stub.description
+	read_aloud_text.add.click()
+
+	read_aloud_edit = ReadAloudTextEditModal(browser)
+	read_aloud_tabs = ReadAloudTextModalTabs(browser)
+
+	read_aloud_table.name.click()
+	import pdb; pdb.set_trace()
+	read_aloud_tabs.edit.click()
+
+	read_aloud_edit.name = stub.name
+	read_aloud_edit.description = stub.description
+	read_aloud_edit.done.click()
+
+	assert read_aloud_table.name.text.strip() == stub.name
+	assert read_aloud_table.description.text.strip() == stub.description
+
+	read_aloud_text.remove.click()
+
+	assert read_aloud_text.add_new_point.text.strip() == 'Add new text for your players.'
